2595248b652b36da88c92cb0800ac5098ad5e40e
galicians <galicians@gmail.com>
2025-11-26 13:54:18 +0000

    header done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,8 @@ import Logo from '../assets/ChefClaudeIcon.png';
 export default function Header() {
   return (
     <header className="header">
-      <nav className="navBar">
-        <img className="header-logo" src={Logo} alt="Logo" />
-        <span>Chef Claude</span>
-      </nav>
+        <img className="header-logo-img" src={Logo} alt="Logo" />
+        <span className='text-logo'>Chef Claude</span>
     </header>
   );
 }
