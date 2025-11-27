7d19dd591790c638458269d5111b0e01611573a7
galicians <galicians@gmail.com>
2025-11-27 16:15:55 +0000

    useState for saving ingredients

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,17 +1,43 @@
+import { useState } from "react";
+
 export default function Main() {
+
+    const ingredients = ['Apple','Rice','Yogurt']
+    const [stateIngredients, setStateIngredients] = useState(ingredients);
+    const listIngredients = stateIngredients.map( ingredient => <li key={ingredient}>{ingredient}</li>)
+
+
+    function addIngredient(event) {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        setStateIngredients(function (prevStateIngredients) {
+            return [...prevStateIngredients,newIngredient]
+        })
+            
+    
+    }
+
+
 
     return (
         <>
         <main>
-            <form className="add-ingredient-form">
+            <form onSubmit={addIngredient} className="add-ingredient-form">
         <input 
             className=""
             type="text"
             placeholder="e.g. oregano"
             aria-label="Add ingredient"
+            name="ingredient"
         ></input>
         <button className="ingredient-input">"Add ingredient</button>
         </form>
+        <div className="ingredients-list-container">
+        <ul>
+            {listIngredients}
+        </ul>
+        </div>
         </main>
         </>
     )
