215c4e6d2641c48420ce9fdf343fa344ec81d1d8
galicians <galicians@gmail.com>
2025-11-27 09:16:38 +0000

    CSS applied to form

---
+++ src/components/Main.jsx
@@ -0,0 +1,18 @@
+export default function Main() {
+
+    return (
+        <>
+        <main>
+            <form className="add-ingredient-form">
+        <input 
+            className=""
+            type="text"
+            placeholder="e.g. oregano"
+            aria-label="Add ingredient"
+        ></input>
+        <button className="ingredient-input">"Add ingredient</button>
+        </form>
+        </main>
+        </>
+    )
+}
